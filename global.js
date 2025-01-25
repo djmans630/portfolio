d82ec410733f29a8fb7df34341d62afce4e6d1b9
djmans630 <dmansperger@ucsd.edu>
2025-01-24 20:09:30 -0800

    a

---
+++ global.js
@@ -0,0 +1,57 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+// let navLinks = $$("nav a");
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+//   );
+// currentLink?.classList.add('current');
+
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact'},
+    { url: 'resume/', title: 'Resume'},
+    { url: "https://github.com/djmans630", title: 'Profile'}
+  ];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    if (!ARE_WE_HOME && !url.startsWith('http')) {
+        url = '../' + url;
+      }
+    let title = p.title;
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+      }
+    
+    if (a.host !== location.host){
+        a.target = "_blank";
+    }
+}
+
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+      <label class="color-scheme">
+      Theme:
+        <select>
+            <option value='light dark'>Automatic</option>
+            <option value='light'>Light</option>
+            <option value='dark'>Dark</option>
+        </select>
+    </label>`
+  );
