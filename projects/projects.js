6bd093d6df8a4f8e6b77bb69a0f08a29f55153b1
djmans630 <dmansperger@ucsd.edu>
2025-01-31 19:13:17 -0800

    1.4

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,9 @@
 import { fetchJSON, renderProjects } from '../global.js';
-const projects = await fetchJSON('../lib/projects.json');
-const projectsContainer = document.querySelector('.projects');
-renderProjects(projects, projectsContainer, 'h2');
+
+async function loadProjects() {
+    const projects = await fetchJSON('../lib/projects.json');
+    const projectsContainer = document.querySelector('.projects');
+    renderProjects(projects, projectsContainer, 'h2');
+}
+
+loadProjects();
