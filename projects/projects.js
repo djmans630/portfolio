c25679817db375cc21e5cb1ad87852b5fb8e2bf2
djmans630 <dmansperger@ucsd.edu>
2025-01-31 19:27:08 -0800

    1.6

--- projects/projects.js
+++ projects/projects.js
@@ -3,6 +3,9 @@ import { fetchJSON, renderProjects } from '../global.js';
 async function loadProjects() {
     const projects = await fetchJSON('../lib/projects.json');
     const projectsContainer = document.querySelector('.projects');
+    const projectsTitle = document.querySelector('.projects-title');
+    const projectText = projects.length === 1 ? 'project' : 'projects';
+    projectsTitle.textContent = `${projects.length} ${projectText}`;
     renderProjects(projects, projectsContainer, 'h2');
 }
 
