ef25efcbef1089cd72649fce2c5d75ba2d617f69
djmans630 <dmansperger@ucsd.edu>
2025-02-07 16:59:56 -0800

    d3 stuff to projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm"
 
 async function loadProjects() {
     const projects = await fetchJSON('../lib/projects.json');
@@ -8,5 +9,11 @@ async function loadProjects() {
     projectsTitle.textContent = `${projects.length} ${projectText}`;
     renderProjects(projects, projectsContainer, 'h2');
 }
+
+let arc = d3.arc().innerRadius(0).outerRadius(50)({
+  startAngle: 0,
+  endAngle: 2 * Math.PI,
+});
+d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
 
 loadProjects();
